e9c6db267c8cb89286d5e318ff991885533acbbe
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-05-23 18:24:19 +0300

    Add Hello

---
+++ 1. Hello Python/lec.py
@@ -0,0 +1,71 @@
+print()
+print('---------- Лекция 1 ----------')
+# print('Hello \nworld \n2342323')
+# a = 2
+# b = 3
+# c = a + b
+# print('a =', a, 'b =',  b, 'c = a + b =', c) # разные стили вывода
+# print('c = {2} b = {1} a = {0}'.format(a, b, c)) # формат
+# print(f'{c} {b} {a}') # интерполяция
+# print('type a =', type (a))
+
+
+# print ('Выводим массив')
+# List = [a, b, c, 'aaaa']
+# print (List)
+
+# print()
+
+# print('Введите f')
+# f = int(input())
+
+# print('Введите d')
+# d = int(input())
+
+# print(f+d)
+
+# print(a/b)    # деление
+# print(a//b)   # целочисленное деление
+# print(a % b)  # остаток от деления
+# print(2**5)   # возведение в степень
+# c = round(a/b, 6) # окргуление до 6 знака
+# print(c)
+
+# a = 3
+# a *= 5 = a * 5 # сокращенный оператор присваивания
+
+# >, >=, <, <=, ==, !=
+# not, and, or – не путать с &, |, ^
+# Кое-что ещё: is, is not, in, not in
+
+# print(a)
+
+# a = 1 > 3
+# print(a)
+
+# a = [1, 3] # сравнение списков
+# b = [1, 4]
+# print(a == b)
+
+# print(3 < 6 > 5) # многоуровневые неравенства
+
+# print(3 < 4 and 5 > 6) # логическое И
+# print(3 < 4 or 5 > 6) # логическое ИЛИ
+
+# f = [1, 2, 3, 4]  # поиск заначение в списке
+# print(2 in f)
+# print(not 3 in f)
+
+# is_odd = not f[0] % 2 # определение четности
+# print(is_odd)
+
+# a = int(input('a = ')) # if / else
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+
+print('----------- Конец ------------')
+print()
