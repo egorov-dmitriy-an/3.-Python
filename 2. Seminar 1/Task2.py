56c53e5ce59de506b2f415c8adc4c2d90db5966d
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-05-24 19:46:59 +0300

    Add edit

--- 2. Seminar 1/Task2.py
+++ 2. Seminar 1/Task2.py
@@ -1,10 +1,8 @@
 print()
-print('---------- Лекция 1 ----------')
-
-list = [0, 0, 0, 0, 0]
+print('------------ Лекция 1 ------------')
+list = []
 for i in range(0, 5):
-    list[i] = int(input(f'Введите элемент {i}: '))
-
+    list.append(int(input(f'Введите элемент {i}: ')))
 print(list)
 
 a = list[0]
@@ -13,6 +11,5 @@ for i in range(1, 5):
     if list[i] > a:
         a = list[i]
 print(f'Максимальное значение равно = {a}')
-print('----------- Конец ------------')
+print('------------- Конец --------------')
 print()
-
