c7b1711e31f95dbc71c2a6bb878275a50f1683af
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-05-26 21:51:02 +0300

    Add Task 3

---
+++ 4. HomeWork Seminar 2/Seminar 2 Task 3.py
@@ -0,0 +1,19 @@
+# Реализуйте алгоритм перемешивания списка.
+print()
+print('---------------- Семинар 2 -----------------')
+import random
+n = int(input('Введите длину списка n = '))
+coll = []
+revers = []
+for i in range(0, n):
+    coll.append(i)
+print(f'Исходный список:     {coll}')
+
+while (n > 0):
+    index = random.randint(0, n-1)
+    revers.append(coll[index])
+    del coll[index]
+    n -= 1
+print(f'Перемешанный список: {revers}')
+print('------------------ Конец -------------------')
+print()
