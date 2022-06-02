ea1de186089254dad0bdd122381977d4f3174aba
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-06-02 19:43:23 +0300

    Add Final

--- 7. HomeWork Seminar 4/Seminar 4 Task 5.py
+++ 7. HomeWork Seminar 4/Seminar 4 Task 5.py
@@ -1,9 +1,6 @@
 # Задача 5. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-from tkinter.font import nametofont
-
-
 x = open('Seminar_4_Task_4_Polynom_1.txt', 'r')
 polynom_1 = x.readline()
 x.close()
@@ -12,31 +9,27 @@ x = open('Seminar_4_Task_4_Polynom_2.txt', 'r')
 polynom_2 = x.readline()
 x.close()
 
-def Polypoly(polynom):
+def Poly_List(polynom):
     coefList = []
+    numb = ''
     i = 0
     while True:
+        s = str(polynom[i])
         if polynom[i] == '^':
             while str(polynom[i+1]).isdigit() == True:
                 polynom = polynom[:i+1] + polynom[i+2:]
         if polynom[i] == '=':
             break
-        i += 1
-
-    polynom = polynom[:-1]
-    
-    numb = ''
-    for i in range(0, len(polynom)):
-        s = str(polynom[i])
         if s.isdigit() == True:
             numb += s
         elif len(numb) != 0:
             coefList.append(int(numb))
             numb = ''
+        i += 1
     return(coefList)
 
-coef_1 = Polypoly(polynom_1)
-coef_2 = Polypoly(polynom_2)
+coef_1 = Poly_List(polynom_1)
+coef_2 = Poly_List(polynom_2)
 
 delta = len(coef_1) - len(coef_2)
 if delta <= 0:
