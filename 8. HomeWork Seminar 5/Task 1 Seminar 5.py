8d3a4cefe9ced5e46dda070c16a481f264246f97
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-06-07 19:10:50 +0300

    Add Task 3

--- 8. HomeWork Seminar 5/Task 1 Seminar 5.py
+++ 8. HomeWork Seminar 5/Task 1 Seminar 5.py
@@ -2,24 +2,7 @@
 print()
 print('--------------------- Семинар 5 ----------------------')
 text = input('Введите текст: ')
-words_text = text.split()
-
-def Find_Liter(original):
-    for i in original:
-        if original.find('а') != -1:
-            return False
-        elif original.find('б') != -1:
-            return False
-        elif original.find('в') != -1:
-            return False
-        else:
-            return True
-
-temp = []
-for i in words_text:
-    if Find_Liter(i) == True:
-        temp.append(i)
-text_new = " ".join(temp)
-print(f'Отсортированный список без слов содержащих "а", "б", "в": {text_new}')
+text = list(filter(lambda x: 'абв' not in x, text.split()))
+print(" ".join(text))
 print('----------------------- Конец ------------------------')
 print()
